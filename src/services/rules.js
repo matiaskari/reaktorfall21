85a6c9a45d3497f44208a55b687e737297358bd7
Matias Kari <46843190+matiaskari@users.noreply.github.com>
2021-08-09 21:04:35 +0300

    rulesService changes

--- src/services/rules.js
+++ src/services/rules.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
-const localProxy = 'https://localhost:8080/'
+const localProxy = 'http://localhost:8080/'
 const myProxy = 'https://gtcorsproxy.herokuapp.com/'
 const baseUrl = 'https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt'
 
 const getRules = () => {
-    const request = axios.get(localProxy + baseUrl, {
+    const request = axios.get(myProxy + baseUrl, {
         headers: {
             'Content-Type': 'text/plain'
         }
